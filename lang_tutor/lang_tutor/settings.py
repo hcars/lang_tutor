f38a89efe5163270484b93a1d9f6cf968dbe7541
Henry Carscadden <hcarscad@gmail.com>
2023-05-07 02:59:12 -0700

    Removed old API Key.

--- lang_tutor/lang_tutor/settings.py
+++ lang_tutor/lang_tutor/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+@j^3qa+&sl+&8f11z11!yca39&j&&zk4_2-wr1o4-br359z3e'
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
