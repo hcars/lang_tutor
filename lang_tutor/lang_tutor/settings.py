e1827276493ab6ad82eff50b42880ff7a43dced8
Henry Carscadden <hcarscad@gmail.com>
2023-06-12 21:24:29 -0700

    Added static root to directory.

--- lang_tutor/lang_tutor/settings.py
+++ lang_tutor/lang_tutor/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'lang_tutor_app/static')]
 # Configures the staticfiles directory to serve
 # static files from /static/ on our deployment
