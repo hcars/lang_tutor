93d81a89b23f9e2ccca89d68c7d2bfa892e0b7e7
Henry Carscadden <hcarscad@gmail.com>
2023-06-12 21:04:00 -0700

    Added static root to directory.

--- lang_tutor/lang_tutor/settings.py
+++ lang_tutor/lang_tutor/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'lang_tutor_app/static')]
 # Configures the staticfiles directory to serve
 # static files from /static/ on our deployment
 STATIC_ROOT = os.path.join(
-    BASE_DIR, 'staticfiles', 'static')
+    BASE_DIR, 'staticfiles', 'lang_tutor_app/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
